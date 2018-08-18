1d86d7d1265fc481b717843197e7f45907676a79
ahaggi <a.haggi@yahoo.com>
2018-08-18 15:26:34 +0200

    1

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    { id: 1, name: "Hero1" },
+    { id: 2, name: "Hero2" },
+    { id: 3, name: "Hero3" },
+    { id: 4, name: "Hero4" },
+    { id: 5, name: "Hero5" },
+    { id: 6, name: "Hero6" },
+    { id: 7, name: "Hero7" },
+    { id: 8, name: "Hero8" },
+    { id: 9, name: "Hero9" },
+    { id: 10, name: "Hero10" }
+]
