ab8f9c756b8adc05dd6db8f0db2b1c7b5a719413
ahaggi <a.haggi@yahoo.com>
2018-08-18 19:40:20 +0200

    implemented hero service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,19 +8,27 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
-  constructor() { }
+
+  /* Reserve the constructor for simple initialization such as wiring constructor parameters to properties. 
+  The constructor shouldn't do anything. It certainly shouldn't call a function that makes HTTP requests to
+   a remote server as a real data service would. Instead, use ngOnInit() for such tasks*/
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
-    // this.selectedHero=this.heroes[0];
+    this.getHeroes();
   }
 
 
-  onSelect(hero:Hero):void{
+  onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
 
   fn(index: number, hero: Hero): number { return hero.id; }
 
