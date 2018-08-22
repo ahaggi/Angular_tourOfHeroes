de9ca479c03b01f0eca84856e492cd08068e5ae5
ahaggi <a.haggi@yahoo.com>
2018-08-22 14:01:24 +0200

    hero-detail.component => onDelete selectedHero = null

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -21,6 +21,8 @@ export class HeroDetailComponent implements OnInit {
 // This component makes a request but it can't actually delete a hero.
   delete() {
     this.deleteRequest.emit(this.heroDetails);
+    this.selectedHero = null;
+
   }
   ngOnInit() {
   }
