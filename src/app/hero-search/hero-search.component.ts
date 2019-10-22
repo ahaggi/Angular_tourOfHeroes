e4d66bc7e403e7b91eb0b5d98affd75e3955d82d
ahaggi <a.haggi@yahoo.com>
2019-10-22 15:29:44 +0200

    HeroSearchComponent: added 2 alternative functions which does not use the  "searchTerms: Subject<string>"

--- src/app/hero-search/hero-search.component.ts
+++ src/app/hero-search/hero-search.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Observable, Subject } from 'rxjs';
+import { Observable, Subject, from, of } from 'rxjs';
 
 import {
-   debounceTime, distinctUntilChanged, switchMap
- } from 'rxjs/operators';
+  debounceTime, distinctUntilChanged, switchMap, filter, tap, defaultIfEmpty
+} from 'rxjs/operators';
 
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
@@ -12,32 +12,73 @@ import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-hero-search',
   templateUrl: './hero-search.component.html',
-  styleUrls: [ './hero-search.component.css' ]
+  styleUrls: ['./hero-search.component.css']
 })
 export class HeroSearchComponent implements OnInit {
   heroes$: Observable<Hero[]>;
   private searchTerms = new Subject<string>();
 
-  constructor(private heroService: HeroService) {}
 
-  // Push a search term into the observable stream.
-  search(term: string): void {
-    this.searchTerms.next(term);
-  }
+  constructor(private heroService: HeroService) { }
+
 
   ngOnInit(): void {
     this.heroes$ = this.searchTerms.pipe(
-      // wait 300ms after each keystroke before considering the term
-      debounceTime(300),
+      // wait 200ms after each keystroke before considering the term
+      debounceTime(200),
 
       // ignore new term if same as previous term
       distinctUntilChanged(),
 
       // switch to new search observable each time the term changes
       switchMap((term: string) => this.heroService.searchHeroes(term)),
-    );
+      tap(h=> console.log(h)),
+
+      );
   }
+
+
+
+    // Push a search term into the subject stream.
+    search(term: string): void {
+      this.searchTerms.next(term);
+    }
+  
+    // search_without_subject_using_from(term: string): void {
+    //   // TODO sjekk hvorfor "from" produserer en feilMelding om det blir tastet "non-Printable Characters"; dvs term == '' ,, MEN "of" fungerer helt greit!
+    //   // dette er for å unngå feilMeldingen.
+    //   let arg = (term && term.length > 0) ? term : " ";
+  
+    //   this.heroes$ = from(arg).pipe(
+  
+    //     // wait 200ms after each keystroke before considering the term
+    //     debounceTime(200),
+  
+    //     // ignore new term if same as previous term
+    //     distinctUntilChanged(),
+  
+    //     // switch to new search observable each time the term changes
+    //     switchMap((term: string) => this.heroService.searchHeroes(term)),
+    //   );
+    // }
+  
+  
+    search_without_subject_using_of(term: string): void {
+      this.heroes$ = of(term).pipe(
+  
+        // wait 200ms after each keystroke before considering the term
+        debounceTime(200),
+  
+        // ignore new term if same as previous term
+        distinctUntilChanged(),
+  
+        // switch to new search observable each time the term changes
+        switchMap((term: string) => this.heroService.searchHeroes(term)),
+      );
+    }
+  
 }
+
 
 
 /* ************** subscribe-vs-async-pipe-in-component-templates  **************
@@ -64,14 +105,14 @@ There are multiple ways in which it is possible to consume this observable strea
       }
     } //class
 
-    template: 
+    template:
     <ul >
     <li *ngFor="let hero of heroes" > OBS heroes ER IKKE OBSERVABLE HER DEN ER VANLIG LIST
       <a routerLink="/detail/{{hero.id}}">
         {{hero.name}}
       </a>
     </li>
-    </ul>   
+    </ul>
 */
 
 
